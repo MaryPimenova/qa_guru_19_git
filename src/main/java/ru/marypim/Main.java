9596ec7ba9eca2ec5276b5dcb60b4a4acb788797
MaryPimenova <129100453+MaryPimenova@users.noreply.github.com>
2023-03-31 09:46:39 +0300

    Update Main.java

--- src/main/java/ru/marypim/Main.java
+++ src/main/java/ru/marypim/Main.java
@@ -1,8 +1,8 @@
 package ru.marypim;
 
 public class Main {
-    private static String msg = "Hello, dear reviewer, you are the best!!!";
+    private static String niceMsg = "Hello, dear reviewer, you are the best!!!";
     public static void main(String[] args) {
-        System.out.println(msg);
+        System.out.println(niceMsg);
     }
 }
